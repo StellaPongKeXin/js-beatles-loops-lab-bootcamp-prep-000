16a99ae8f62964a46e99e31c7b2fe29d826e5db2
STELLA PONG KEXIN <stellapong0513@hotmail.com>
2017-11-04 11:33:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
  const singers = ["John Lennon", "Ringo Star", "Paul McCartney", "Dizzy"];
  const instruments = ["guitar", "drums", "piano", "trumpet"];
- 
- 
+
+
  var theBeatlesPlay = function (musiciansArray, instrumentsArray) {
  	var collect = [];
  	for (var i = 0; i<musiciansArray.length; i++) {
@@ -10,21 +10,21 @@
  	}
  	return(collect);
  };
- 
+
  theBeatlesPlay(singers, instruments);
- 
+
  var facts = [
  	"He was the last Beatle to learn to drive",
  	"He was never a vegetarian",
  	"He was a choir boy and boy scout",
  	"He hated the sound of his own voice"
  ];
- 
+
  var johnLennonFacts = function (factsArray) {
  	var i = 0;
  	var arrayCounter = factsArray.length
  	var newFactsArray = [];
- 
+
  	while (arrayCounter > 0) {
  		newFactsArray.push(factsArray[i] + "!!!");
  		i++;
@@ -32,25 +32,25 @@
  	}
  	return(newFactsArray);
  };
- 
+
  johnLennonFacts(facts);
- 
+
  var iLoveTheBeatles = function (num){
  	var arr = [];
  	num++;
- 
+
  	if (num === 18) {
  		return("I love the Beatles!");
  	}
- 
+
  	var i = 0;
  	do {
  		arr.push("I love the Beatles!");
  		i++;
- 
+
  	} while (i < num);
- 
+
  	return(arr);
  }
- 
+
 iLoveTheBeatles(2);
